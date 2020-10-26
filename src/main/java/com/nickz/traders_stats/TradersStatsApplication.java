c2cb1ca3856f6dcd125d26199bcac8901e993fe6
Nikolay <nick.zalosko@gmail.com>
2020-10-26 20:07:42 +0300

    init

---
+++ src/main/java/com/nickz/traders_stats/TradersStatsApplication.java
@@ -0,0 +1,13 @@
+package com.nickz.traders_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradersStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradersStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nickz/traders_stats/TradersStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickz.traders_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradersStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
